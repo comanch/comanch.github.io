64e52ba5f3f907ccd325d5622c1ca3007fd5a7fd
Roman <nro91kh@gmail.com>
2020-08-10 23:40:02 +0300

    1

---
+++ todo/precache-manifest.bb2299fd3934bf487c3a5736fdbf03f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9815ab8f20c9973740427aabb6586cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f2e3e4e0120ab739b2",
+    "url": "/static/css/main.6af88632.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f2e3e4e0120ab739b2",
+    "url": "/static/js/main.d5f94cd1.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
